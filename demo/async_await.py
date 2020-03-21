da466b229e53704fbc213a6ae907a129bcdc56c8
Li, Guoliang. FR <Guoliang.Li@fr.bertrandt.com>
2020-03-21 23:22:10 +0100

    async

--- demo/async_await.py
+++ demo/async_await.py
@@ -1,20 +1,27 @@
 #python为了将语义变得更加明确，就引入了async和await关键词用于定义原生的协程
-# async def downloader(url):
-#     return "bobby"
+
 import types
 
-@types.coroutine
-def downloader(url):
-    yield "bobby"
+async def downloader(url):
+    return "bobby"
+
+# @types.coroutine
+# def downloader(url):
+#     yield "bobby"
 
 async def download_url(url):
     #dosomethings
     html = await downloader(url)
+    print(html)
     return html
 
 if __name__ == "__main__":
     coro = download_url("http://www.imooc.com")
-    # next(None)
-    coro.send(None)
+    # next(coro)
+    try:
+        coro.send(None)
+    except :
+        pass
+    # print(coro)
 
 
--- demo/yield_from_example.py
+++ demo/yield_from_example.py
@@ -35,19 +35,19 @@ def main():
 
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
 
 
 
-# if __name__ == "__main__":
-#     my_gen = sales_sum("bobby牌手机")
-#     my_gen.send(None)
-#     my_gen.send(1200)
-#     my_gen.send(1500)
-#     my_gen.send(3000)
-#     try:
-#         my_gen.send(None)
-#     except StopIteration as e:
-#         result = e.value
-#         print(result)
+if __name__ == "__main__":
+    my_gen = sales_sum("bobby牌手机")
+    my_gen.send(None)
+    my_gen.send(1200)
+    my_gen.send(1500)
+    my_gen.send(3000)
+    try:
+        my_gen.send(None)
+    except StopIteration as e:
+        result = e.value
+        print(result)
