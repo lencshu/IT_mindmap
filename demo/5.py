eb97ab3774382a825ef3a9d318337943e5f711e2
Li, Guoliang. FR <Guoliang.Li@fr.bertrandt.com>
2020-03-21 22:32:36 +0100

    yield from

--- demo/5.py
+++ demo/5.py
@@ -1,1 +1,12 @@
-import collections.abc
+def gen_func():
+    html=yield 'ggggg'
+    print(html)
+    yield 2
+
+if __name__=="__main__":
+    gen=gen_func()
+    url = next(gen)
+    print(url)
+    gen.send('hhh')
+
+    pass
